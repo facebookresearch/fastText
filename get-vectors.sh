c600ed647249d68ac88ed84a48533baaf7ac18b5
Piotr Bojanowski <bojanowski@fb.com>
2016-07-22 09:24:11 +0200

    Changed default amount of threads in get-vectors.sh

--- get-vectors.sh
+++ get-vectors.sh
@@ -12,7 +12,7 @@ mkdir -p result
 
 ./fasttext -input data/text9 -output result/text9 -lr 0.025 -dim 50 \
   -ws 5 -epoch 1 -minCount 5 -neg 5 -sampling sqrt -loss ns -model sg \
-  -bucket 2000000 -minn 3 -maxn 6 -onlyWord 0 -thread 40 -verbose 1000 \
+  -bucket 2000000 -minn 3 -maxn 6 -onlyWord 0 -thread 8 -verbose 1000 \
   -t 1e-4
 
 cut -f 1,2 data/rw/rw.txt | awk '{print tolower($0)}' | tr '\t' '\n' > data/queries.txt
