8a934fff15afe9622d7e17b5fedfe8a9ed2bda43
Piotr Bojanowski <bojanowski@fb.com>
2016-07-25 17:31:26 +0200

    added a variable in sh files to specify the data and results folder

--- get-vectors.sh
+++ get-vectors.sh
@@ -16,15 +16,18 @@ make
 
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 
-mkdir -p result
+RESULTDIR=result
+DATADIR=data
 
-./fasttext -input data/text9 -output result/text9 -lr 0.025 -dim 300 \
+mkdir -p "${RESULTDIR}"
+
+./fasttext -input "${DATADIR}"/text9 -output "${RESULTDIR}"/text9 -lr 0.025 -dim 300 \
   -ws 5 -epoch 1 -minCount 5 -neg 5 -sampling sqrt -loss ns -model sg \
   -bucket 2000000 -minn 3 -maxn 6 -onlyWord 0 -thread 8 -verbose 1000 \
   -t 1e-4
 
-cut -f 1,2 data/rw/rw.txt | awk '{print tolower($0)}' | tr '\t' '\n' > data/queries.txt
+cut -f 1,2 "${DATADIR}"/rw/rw.txt | awk '{print tolower($0)}' | tr '\t' '\n' > "${DATADIR}"/queries.txt
 
-cat data/queries.txt | ./print-vectors result/text9.bin > result/vectors.txt
+cat "${DATADIR}"/queries.txt | ./print-vectors "${RESULTDIR}"/text9.bin > "${RESULTDIR}"/vectors.txt
 
-python eval.py -m result/vectors.txt -d data/rw/rw.txt
+python eval.py -m "${RESULTDIR}"/vectors.txt -d "${DATADIR}"/rw/rw.txt
