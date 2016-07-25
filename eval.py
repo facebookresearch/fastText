b44e9a3a520d42658f636fd14067f393911fad24
Piotr Bojanowski <bojanowski@fb.com>
2016-07-25 16:53:33 +0200

    added headers to py and sh files

--- eval.py
+++ eval.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# 
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+# 
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
 
 from __future__ import absolute_import
 from __future__ import division
--- get-vectors.sh
+++ get-vectors.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# 
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+# 
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+# 
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -10,7 +18,7 @@ export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 
 mkdir -p result
 
-./fasttext -input data/text9 -output result/text9 -lr 0.025 -dim 50 \
+./fasttext -input data/text9 -output result/text9 -lr 0.025 -dim 300 \
   -ws 5 -epoch 1 -minCount 5 -neg 5 -sampling sqrt -loss ns -model sg \
   -bucket 2000000 -minn 3 -maxn 6 -onlyWord 0 -thread 8 -verbose 1000 \
   -t 1e-4
