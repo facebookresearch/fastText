73d12c4ac8e3c0248adbf9e33628402e9ff163f8
Piotr Bojanowski <bojanowski@fb.com>
2016-07-29 15:25:51 +0200

    Get data now downloads yelp review full and ab_news. Wrote a script for sentence classification. Wrote a function predict to predict most likely label at test time.

--- get-data.sh
+++ get-data.sh
@@ -8,19 +8,45 @@
 # of patent rights can be found in the PATENTS file in the same directory.
 # 
 
+normalize_text() {
+  sed -e 's/^"//g;s/"$//g;s/","/ /g' | awk '{print "__label__" $0}' | awk '{print tolower($0);}' | \
+         sed -e "s/’/'/g" -e "s/′/'/g" -e "s/''/ /g" -e "s/'/ ' /g" -e "s/“/\"/g" -e "s/”/\"/g" \
+             -e 's/"/ " /g' -e 's/\./ \. /g' -e 's/<br \/>/ /g' -e 's/, / , /g' -e 's/(/ ( /g' -e 's/)/ ) /g' -e 's/\!/ \! /g' \
+             -e 's/\?/ \? /g' -e 's/\;/ /g' -e 's/\:/ /g' -e 's/-/ - /g' -e 's/=/ /g' -e 's/=/ /g' -e 's/*/ /g' -e 's/|/ /g' \
+             -e 's/«/ /g' | tr -s " "
+}
+
 DATADIR=data
 
 mkdir -p "${DATADIR}"
 
-if [ ! -f "${DATADIR}"/text9 ]
+if [ ! -f "${DATADIR}/text9" ]
 then
   wget -c http://mattmahoney.net/dc/enwik9.zip -P "${DATADIR}"
-  unzip "${DATADIR}"/enwik9.zip -d "${DATADIR}"
-  perl wikifil.pl "${DATADIR}"/enwik9 > "${DATADIR}"/text9
+  unzip "${DATADIR}/enwik9.zip" -d "${DATADIR}"
+  perl wikifil.pl "${DATADIR}/enwik9" > "${DATADIR}"/text9
 fi
 
-if [ ! -f "${DATADIR}"/rw/rw.txt ]
+if [ ! -f "${DATADIR}/rw/rw.txt" ]
 then
   wget -c http://www-nlp.stanford.edu/~lmthang/morphoNLM/rw.zip -P "${DATADIR}"
-  unzip "${DATADIR}"/rw.zip -d "${DATADIR}"
+  unzip "${DATADIR}/rw.zip" -d "${DATADIR}"
 fi
+
+if [ ! -f "${DATADIR}/ag_news.train" ]
+then
+  wget 'https://googledrive.com/host/0Bz8a_Dbh9QhbUDNpeUdjb0wxRms' -O "${DATADIR}/ag_news_csv.tar.gz"
+  tar -xzvf "${DATADIR}/ag_news_csv.tar.gz" -C "${DATADIR}"
+  cat "${DATADIR}/ag_news_csv/train.csv" | normalize_text > "${DATADIR}/ag_news.train"
+  cat "${DATADIR}/ag_news_csv/test.csv" | normalize_text > "${DATADIR}/ag_news.test"
+fi
+
+if [ ! -f "${DATADIR}/yelp_review_full" ]
+then
+  wget 'https://googledrive.com/host/0Bz8a_Dbh9QhbZlU4dXhHTFhZQU0' -O "${DATADIR}/yelp_review_full_csv.tar.gz"
+  tar -xzvf "${DATADIR}/yelp_review_full_csv.tar.gz" -C "${DATADIR}"
+  cat "${DATADIR}/yelp_review_full_csv/train.csv" | normalize_text > "${DATADIR}/yelp_review_full.train"
+  cat "${DATADIR}/yelp_review_full_csv/test.csv" | normalize_text > "${DATADIR}/yelp_review_full.test"
+fi
+
+
--- get-vectors.sh
+++ get-vectors.sh
@@ -21,9 +21,9 @@ DATADIR=data
 
 mkdir -p "${RESULTDIR}"
 
-./fasttext skipgram -input "${DATADIR}"/text9 -output "${RESULTDIR}"/text9 -lr 0.025 -dim 300 \
+./fasttext skipgram -input "${DATADIR}"/text9 -output "${RESULTDIR}"/text9 -lr 0.025 -dim 100 \
   -ws 5 -epoch 1 -minCount 5 -neg 5 -sampling sqrt -loss ns \
-  -bucket 2000000 -minn 3 -maxn 6 -onlyWord 0 -thread 8 -verbose 1000 \
+  -bucket 2000000 -minn 3 -maxn 6 -onlyWord 0 -thread 4 -verbose 1000 \
   -t 1e-4
 
 cut -f 1,2 "${DATADIR}"/rw/rw.txt | awk '{print tolower($0)}' | tr '\t' '\n' > "${DATADIR}"/queries.txt
