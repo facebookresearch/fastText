debabc57f2b5b24818b5d56199e2c36277d3aca1
Stephane Bonneaud <stephane.bonneaud@gmail.com>
2017-02-14 20:18:52 -0800

    Making test script work for the new test and predict methods changed in the previous commit.

--- tests/test_granularities.sh
+++ tests/test_granularities.sh
@@ -11,15 +11,17 @@
 RESULTDIR=result
 DATADIR=.
 
-DATAFILE=data0
+DATAFILE=data1
 
 mkdir -p "${RESULTDIR}"
 mkdir -p "${DATADIR}"
 
 make -C ..
 
-../fasttext supervised -input "${DATADIR}/${DATAFILE}.train" -output "${RESULTDIR}/${DATAFILE}" -dim 10 -granularities 3 -lr 0.1 -wordNgrams 2 -minCount 1 -bucket 10000000 -epoch 5 -thread 4
+GRANULARITIES=3
 
-# ../fasttext test "${RESULTDIR}/${DATAFILE}.bin" "${DATADIR}/${DATAFILE}.test"
+../fasttext supervised -input "${DATADIR}/${DATAFILE}.train" -output "${RESULTDIR}/${DATAFILE}" -dim 10 -granularities ${GRANULARITIES} -lr 0.1 -wordNgrams 2 -minCount 1 -bucket 10000000 -epoch 5 -thread 4
 
-# ../fasttext predict "${RESULTDIR}/${DATAFILE}.bin" "${DATADIR}/${DATAFILE}.test" > "${RESULTDIR}/${DATAFILE}.test.predict"
+../fasttext test "${RESULTDIR}/${DATAFILE}.bin" "${DATADIR}/${DATAFILE}.test" ${GRANULARITIES}
+
+../fasttext predict "${RESULTDIR}/${DATAFILE}.bin" "${DATADIR}/${DATAFILE}.test" > "${RESULTDIR}/${DATAFILE}.test.predict" ${GRANULARITIES}
