712d7ab27b959866c6a1bbce284c819c920d88db
Jaan Altosaar <jaan.altosaar@gmail.com>
2017-03-14 15:23:35 -0400

    test recipe embeddings

--- sentence-context-example.sh
+++ sentence-context-example.sh
@@ -7,10 +7,10 @@ mkdir -p "${RESULTDIR}"
 
 make
 
-./fasttext skipgram -input "${DATADIR}"/dat/processed/kaggle_and_nature_train -output "${RESULTDIR}"/model -lr 0.025 -dim 100 \
+./fasttext sentence_context -input "${DATADIR}"/dat/processed/kaggle_and_nature_train -output "${RESULTDIR}"/model -lr 0.025 -dim 100 \
   -ws 0 -epoch 3 -minCount 5 -neg 5 -loss ns -bucket 2000000 \
   -wordNgrams 0 \
-  -minn 1 -maxn 6 -thread 4 -t 0 -lrUpdateRate 100
+  -minn 0 -maxn 0 -thread 8 -t 1 -lrUpdateRate 100
 
 cat "${DATADIR}"/fit/nature_and_kaggle_vocab_words.txt | ./fasttext print-vectors "${RESULTDIR}"/model.bin > "${RESULTDIR}"/vectors.txt
 
