00f5168de5042f1c11be394e538c1602b5d1438c
Jaan Altosaar <jaan.altosaar@gmail.com>
2017-03-15 16:06:26 -0400

    fix printing of model vectors

--- sentence-context-example.sh
+++ sentence-context-example.sh
@@ -7,11 +7,11 @@ mkdir -p "${RESULTDIR}"
 
 make
 
-./fasttext sentence_context -input "${DATADIR}"/dat/processed/kaggle_and_nature_train -output "${RESULTDIR}"/model -lr 0.025 -dim 100 \
-  -ws 0 -epoch 3 -minCount 5 -neg 5 -loss ns -bucket 2000000 \
+./fasttext sentence_context -input "${DATADIR}"/dat/processed/train -output "${RESULTDIR}"/model -lr 0.025 -dim 100 \
+  -ws 0 -epoch 1 -minCount 5 -neg 5 -loss ns -bucket 2000000 \
   -wordNgrams 0 \
   -minn 0 -maxn 0 -thread 8 -t 1 -lrUpdateRate 100
 
-cat "${DATADIR}"/fit/nature_and_kaggle_vocab_words.txt | ./fasttext print-vectors "${RESULTDIR}"/model.bin > "${RESULTDIR}"/vectors.txt
+tail -n+3 $RESULTDIR/model.vec > $RESULTDIR/vectors.txt
 
 python eval_interactive.py -m "${RESULTDIR}"/vectors.txt
