426878390e80b1b86dadf72445b81dacca31445a
Christian Puhrsch <cpuhrsch@fb.com>
2018-01-02 12:56:31 -0800

    setup.py OS X compiler flags
    Summary: Change flags to better support clang
    
    Reviewed By: kahne
    
    Differential Revision: D6651377
    
    fbshipit-source-id: 5e8fb9362fe934edf75cdcea3bf87f96dff56c95

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import sys
 import setuptools
 import os
 
-__version__ = '0.8.21'
+__version__ = '0.8.22'
 FASTTEXT_SRC = "src"
 
 # Based on https://github.com/pybind/python_example
@@ -66,7 +66,7 @@ ext_modules = [
 
 # As of Python 3.6, CCompiler has a `has_flag` method.
 # cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
+def has_flag(compiler, flags):
     """Return a boolean indicating whether a flag name is supported on
     the specified compiler.
     """
@@ -74,7 +74,7 @@ def has_flag(compiler, flagname):
     with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
         f.write('int main (int argc, char **argv) { return 0; }')
         try:
-            compiler.compile([f.name], extra_postargs=[flagname])
+            compiler.compile([f.name], extra_postargs=flags)
         except setuptools.distutils.errors.CompileError:
             return False
     return True
@@ -84,17 +84,14 @@ def cpp_flag(compiler):
     """Return the -std=c++[0x/11/14] compiler flag.
     The c++14 is preferred over c++0x/11 (when it is available).
     """
-    if has_flag(compiler, '-std=c++14'):
-        return '-std=c++14'
-    elif has_flag(compiler, '-std=c++11'):
-        return '-std=c++11'
-    elif has_flag(compiler, '-std=c++0x'):
-        return '-std=c++0x'
-    else:
-        raise RuntimeError(
-            'Unsupported compiler -- at least C++0x support '
-            'is needed!'
-        )
+    standards = ['-std=c++14', '-std=c++11', '-std=c++0x']
+    for standard in standards:
+        if has_flag(compiler, [standard]):
+            return standard
+    raise RuntimeError(
+        'Unsupported compiler -- at least C++0x support '
+        'is needed!'
+    )
 
 
 class BuildExt(build_ext):
@@ -106,15 +103,22 @@ class BuildExt(build_ext):
 
     def build_extensions(self):
         if sys.platform == 'darwin':
-            if has_flag(self.compiler, '-stdlib=libc++'):
-                self.c_opts['unix'] += ['-stdlib=libc++']
-            self.c_opts['unix'] += ['-mmacosx-version-min=10.7']
+            all_flags = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+            if has_flag(self.compiler, [all_flags[0]]):
+                self.c_opts['unix'] += [all_flags[0]]
+            elif has_flag(self.compiler, all_flags):
+                self.c_opts['unix'] += all_flags
+            else:
+                raise RuntimeError(
+                    'libc++ is needed! Failed to compile with {} and {}.'.
+                    format(" ".join(all_flags), all_flags[0])
+                )
         ct = self.compiler.compiler_type
         opts = self.c_opts.get(ct, [])
         if ct == 'unix':
             opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
             opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
+            if has_flag(self.compiler, ['-fvisibility=hidden']):
                 opts.append('-fvisibility=hidden')
         elif ct == 'msvc':
             opts.append(
