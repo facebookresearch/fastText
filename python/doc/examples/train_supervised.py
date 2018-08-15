b589d286e28998119a0beab835c6a25c1b0c7f81
bact <arthit@gmail.com>
2018-08-15 13:29:19 -0700

    Remove duplicated imports in train_supervised.py
    Summary: Pull Request resolved: https://github.com/facebookresearch/fastText/pull/524
    
    Differential Revision: D9348102
    
    Pulled By: EdouardGrave
    
    fbshipit-source-id: 6fa88544517652f184254238d4129da9c5b734ce

--- python/doc/examples/train_supervised.py
+++ python/doc/examples/train_supervised.py
@@ -11,7 +11,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from __future__ import division, absolute_import, print_function
 
 import os
 from fastText import train_supervised
