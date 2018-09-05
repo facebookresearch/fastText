31acb06467067883a0c61d877cb40c22b20fe3ab
Dan Melnic <dmm@fb.com>
2018-09-05 16:27:39 -0700

    Initialize uninitialized std::atomic variables
    Summary: Initialize uninitialized std::atomic variables
    
    Reviewed By: WillerZ
    
    Differential Revision: D9661325
    
    fbshipit-source-id: d515b672b81d03a9e82e6a1ffb448ecca3518608

--- src/fasttext.h
+++ src/fasttext.h
@@ -43,8 +43,8 @@ class FastText {
 
   std::shared_ptr<Model> model_;
 
-  std::atomic<int64_t> tokenCount_;
-  std::atomic<real> loss_;
+  std::atomic<int64_t> tokenCount_{};
+  std::atomic<real> loss_{};
 
   std::chrono::steady_clock::time_point start_;
   void signModel(std::ostream&);
