deb8fa0122c600d18771b3b2fcb0facae105bb04
Junjie Qian <juqia@microsoft.com>
2019-06-26 14:40:18 -0700

    better default thread number of c++ impl

--- src/args.cc
+++ src/args.cc
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <thread>
 
 namespace fasttext {
 
@@ -29,7 +30,7 @@ Args::Args() {
   bucket = 2000000;
   minn = 3;
   maxn = 6;
-  thread = 12;
+  thread = (std::thread::hardware_concurrency() - 1 > 0) ? (std::thread::hardware_concurrency() - 1) : 12 ;
   lrUpdateRate = 100;
   t = 1e-4;
   label = "__label__";
