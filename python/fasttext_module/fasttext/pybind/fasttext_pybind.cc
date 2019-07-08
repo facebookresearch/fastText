074e2402a63715e7c382e701c7b77e37cafe96e1
Luke <lukeandshuo@sina.com>
2019-07-08 16:29:26 -0700

    fix python2 memory leak

--- python/fasttext_module/fasttext/pybind/fasttext_pybind.cc
+++ python/fasttext_module/fasttext/pybind/fasttext_pybind.cc
@@ -31,7 +31,9 @@ py::str castToPythonString(const std::string& s, const char* onUnicodeError) {
   // for python 2 and not encoded for python 3 :
   // https://github.com/pybind/pybind11/blob/ccbe68b084806dece5863437a7dc93de20bd9b15/include/pybind11/pytypes.h#L930
 #if PY_MAJOR_VERSION < 3
-  handle = PyUnicode_AsEncodedString(handle, "utf-8", onUnicodeError);
+  PyObject* handle_encoded = PyUnicode_AsEncodedString(handle, "utf-8", onUnicodeError);
+  Py_DECREF(handle); 
+  handle = handle_encoded;
 #endif
 
   py::str handle_str = py::str(handle);
